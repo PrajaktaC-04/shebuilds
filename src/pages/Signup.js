297b1e9968125960f409c5ee536578e6b0773c14
Prajakta Sandeep Chavan <110226806+PrajaktaC-04@users.noreply.github.com>
2023-01-02 11:59:41 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 const Signup = () => {
     return (
-    <h1>Signup Me</h1>
+    <>
+      <div className="page">
+            <div className="main-box">
+                <h1>SignUp</h1>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Repeat your password" />
+
+                <div className="login-btn">Signup</div>
+                <div className="new-sign">
+                <p className="text">Already have an acoount?</p>
+                <NavLink className="loginlink" to= "/login">Login</NavLink>
+                </div>
+            
+            </div>
+        </div>
+    </>
     );
   };
 
